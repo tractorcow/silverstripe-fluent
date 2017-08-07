0da2694890108b768a0f025736ec8db94da37033
Damian Mooyman <damian@silverstripe.com>
2017-08-07 18:17:20 +1200

    Stash of WIP for building the locale menu

--- client/dist/js/fluent.js
+++ client/dist/js/fluent.js
@@ -1,1 +1,141 @@
-!function(n){function e(i){if(t[i])return t[i].exports;var o=t[i]={i:i,l:!1,exports:{}};return n[i].call(o.exports,o,o.exports,e),o.l=!0,o.exports}var t={};e.m=n,e.c=t,e.i=function(n){return n},e.d=function(n,t,i){e.o(n,t)||Object.defineProperty(n,t,{configurable:!1,enumerable:!0,get:i})},e.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return e.d(t,"a",t),t},e.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},e.p="",e(e.s=0)}([function(n,e){window.jQuery.entwine("ss",function(n){n("input[data-hides]").entwine({onmatch:function(){this._super();var e=this.data("hides"),t=n("[name='"+e+"']").closest(".field");this.is(":checked")?t.hide():t.show()},onunmatch:function(){this._super()},onchange:function(){var e=this.data("hides"),t=n("[name='"+e+"']").closest(".field");this.is(":checked")?t.slideUp():t.slideDown()}})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+window.jQuery.entwine('ss', function ($) {
+  $('input[data-hides]').entwine({
+    onmatch: function onmatch() {
+      this._super();
+      var hideName = this.data('hides');
+      var target = $('[name=\'' + hideName + '\']').closest('.field');
+      if (this.is(':checked')) {
+        target.hide();
+      } else {
+        target.show();
+      }
+    },
+    onunmatch: function onunmatch() {
+      this._super();
+    },
+    onchange: function onchange() {
+      var hideName = this.data('hides');
+      var target = $('[name=\'' + hideName + '\']').closest('.field');
+      if (this.is(':checked')) {
+        target.slideUp();
+      } else {
+        target.slideDown();
+      }
+    }
+  });
+
+  $('.cms > .cms-container > .cms-menu > .cms-panel-content').entwine({
+    fluentConfig: function fluentConfig() {
+      var section = 'TractorCow\\Fluent\\Control\\LocaleAdmin';
+      if (window && window.ss && window.ss.config && window.ss.config.sections) {
+        var config = window.ss.config.sections.find(function (next) {
+          return next.name === section;
+        });
+        if (config) {
+          return config.fluent || {};
+        }
+      }
+      return {};
+    },
+    onmatch: function onmatch() {
+      this._super();
+      var config = this.fluentConfig();
+
+      if (typeof config.locales === 'undefined' || typeof config.title === 'undefined') {
+        return;
+      }
+      var buttonTitle = config.title;
+      var selector = $('<div class=\'cms-fluent-selector\'>\n          <span class=\'icon icon-16 icon-fluent-translate\'>&nbsp;</span>\n          <span class=\'text\'></span>\n          <a class=\'cms-fluent-selector-flydown\' type=\'button\'>\n            <span class=\'icon icon-fluent-select\'></span>\n          </a>\n          <ul class=\'cms-fluent-selector-locales\'></ul>\n        </div>');
+      $('.cms-fluent-selector-flydown', selector).prop('title', buttonTitle);
+      $('.cms-fluent-selector-flydown span', selector).text(buttonTitle);
+
+      config.locales.forEach(function (locale) {
+        var item = $("<li><a><span class='full-title'></span><span class='short-title'></span></a></li>");
+        $('.full-title', item).text(locale.title);
+        $('.short-title', item).text(locale.code.split('_')[0]);
+        $('a', item).attr('data-locale', locale.code).attr('title', locale.title);
+        $('.cms-fluent-selector-locales', selector).append(item);
+      });
+
+      $('.text', selector).text(config.locales[config.locale]);
+
+      this.prepend(selector);
+    }
+  });
+});
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=fluent.js.map
